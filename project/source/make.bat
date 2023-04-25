4be4bda1252daf26cd07ec7d9f8a6ef66d13497a
Sashauxgit <sashaux@yandex.ru>
2023-04-25 15:00:10 +0300

    Окончательная версия проекта, работающего на linux и windows. 'Запушино с linux'

--- project/source/make.bat
+++ project/source/make.bat
@@ -1,4 +1,4 @@
 ::chcp 866
 del HaffmanCoder.exe
-g++ -std=c++20 main.cpp StreamType.cpp Binfstream.cpp HaffCoder.cpp HaffNode.cpp IntermediaData.cpp -o HaffmanCoder.exe
+g++ -std=c++20 source\main.cpp source\StreamType.cpp source\Binfstream.cpp source\HaffCoder.cpp source\HaffNode.cpp source\IntermediaData.cpp -o HaffmanCoder.exe
 HaffmanCoder.exe --help
